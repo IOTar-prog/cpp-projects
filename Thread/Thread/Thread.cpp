4407b00b16660937eef0b1cbeee3046d7e0b3da5
IOTar-prog <70750411+IOTar-prog@users.noreply.github.com>
2024-03-05 00:43:58 +0300

    vector

--- Thread/Thread/Thread.cpp
+++ Thread/Thread/Thread.cpp
@@ -9,7 +9,7 @@ mutex mut;
 //Функция для создания массива
 void arr(int* arr, int size) {
 	for (int i = 0; i < size; i++) {
-		arr[i] = i + 1;
+		arr[i] = 1;
 	}
 }
 
@@ -25,16 +25,14 @@ int arr_sum(int* arr, int size) {
 //Подсчёт суммы элементов масива с начального по конечный индекс
 void counter(int& sum, int* arr, int size, int begin_index, int end_index) {
 	for (int i = begin_index; i < end_index; i++) {
-		mut.lock();
 		sum += arr[i];
-		mut.unlock();
 	}
 }
 
 int main()
 {
 	// Размер массива
-	int arr_size = 100000;
+	int arr_size = 10000000;
 
 	//Создаём массив
 	int* a = new int[arr_size];
@@ -47,18 +45,21 @@ int main()
 	clock_t std_end = clock();
 
 	//Переменная для хранения результатов вычесления потоков
-	int thrd_sum = 0;
+	int thrd1_sum = 0;
+	int thrd2_sum = 0;
 
 	clock_t thrd_start = clock();
 	//Первый поток
-	thread thread1{ counter,std::ref(thrd_sum), a, arr_size, 0, (arr_size / 2) };
-	thread1.join();
+	thread thread1{ counter,std::ref(thrd1_sum), a, arr_size, 0, (arr_size / 2) };
+	
 
 	//Второй поток
-	thread thread2{ counter,std::ref(thrd_sum), a, arr_size, (arr_size / 2), arr_size };
-	thread2.join();
+	thread thread2{ counter,std::ref(thrd2_sum), a, arr_size, (arr_size / 2), arr_size };
+	
 
-	cout << "Thrd_sum = " << thrd_sum << "\n";
+	thread1.join();
+	thread2.join();
+	cout << "Thrd_sum = " << thrd1_sum+thrd2_sum << "\n";
 	clock_t thrd_end = clock();
 
 	double std_time_sec = (double)(std_end - std_start) / CLOCKS_PER_SEC;
---
+++ Thread_vector/vector/vector.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <mutex>
+#include <thread>
+#include <time.h>
+#include <vector>
+
+using namespace std;
+mutex mut;
+
+//Функция для создания массива
+void arr_create(unsigned long long* arr, unsigned long long size) {
+	for (unsigned long long i = 0; i < size; i++) {
+		arr[i] = 1;
+	}
+}
+
+//Подсчёт суммы элементов массива с начального по конечный индекс
+void sum(unsigned long long& result, unsigned long long* arr, unsigned long long begin_index, unsigned long long end_index) {
+	for (unsigned long long i = begin_index; i < end_index; i++) {
+		mut.lock();
+		result += arr[i];
+		mut.unlock();
+	}
+}
+
+int main() {
+
+	unsigned long long arr_size = 200;
+	unsigned long long result = 0;
+	int num_of_thrd = 10;
+
+	//Создаём массив
+	unsigned long long* arr = new unsigned long long[arr_size];
+	arr_create(arr, arr_size);
+
+	//Определяем количество потоков
+	/*
+	cout << "Num of threads: ";
+	cin >> num_of_thrd;
+	*/
+
+	//проверяем остаток от деления числа элементов массива на кол-во потоков
+	if (arr_size % num_of_thrd != 0) {
+		int n = sizeof(arr) / sizeof(arr[0]);
+		for (int i = 0; i < arr_size % num_of_thrd; i++) {
+			result += arr[n - 1];
+		}
+
+		for (int i = 0; i < (sizeof(arr) / sizeof(arr[0])); i++) {
+			arr_size -= 1;
+		}
+	}
+
+	vector<thread> threads;
+	for (int i = 0; i < num_of_thrd; i++) {
+
+		if (i == 0) {
+			threads.emplace_back(thread(sum, std::ref(result), arr, 0, (arr_size / num_of_thrd)));
+		}
+		else {
+			unsigned long long start = (arr_size / num_of_thrd) * i;
+			unsigned long long end_id = (start + arr_size / num_of_thrd);
+
+			threads.emplace_back(thread(sum, std::ref(result), arr, start, end_id));
+		}
+
+	}
+
+	for (int i = 0; i < num_of_thrd; ++i) {
+		threads.at(i).join();
+	}
+
+	delete[] arr;
+	cout << result;
+
+}
