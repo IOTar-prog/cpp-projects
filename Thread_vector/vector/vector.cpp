802708fb2c2cb6d51c9d66699b00fc6dd9570716
IOTar-prog <70750411+IOTar-prog@users.noreply.github.com>
2024-03-05 11:16:02 +0300

    vector threads

--- Thread_vector/vector/vector.cpp
+++ Thread_vector/vector/vector.cpp
@@ -25,21 +25,20 @@ void sum(unsigned long long& result, unsigned long long* arr, unsigned long long
 
 int main() {
 
-	unsigned long long arr_size = 200;
+	unsigned long long arr_size = 2000022;
 	unsigned long long result = 0;
-	int num_of_thrd = 10;
+	int num_of_thrd = 0;
 
 	//Создаём массив
 	unsigned long long* arr = new unsigned long long[arr_size];
 	arr_create(arr, arr_size);
 
 	//Определяем количество потоков
-	/*
 	cout << "Num of threads: ";
 	cin >> num_of_thrd;
-	*/
 
-	//проверяем остаток от деления числа элементов массива на кол-во потоков
+
+	//Проверяем остаток от деления числа элементов массива на кол-во потоков
 	if (arr_size % num_of_thrd != 0) {
 		int n = sizeof(arr) / sizeof(arr[0]);
 		for (int i = 0; i < arr_size % num_of_thrd; i++) {
@@ -51,7 +50,9 @@ int main() {
 		}
 	}
 
+	//Создаём контейнер для хранения потоков и параллельно считаем сумму
 	vector<thread> threads;
+	clock_t thrd_start_time = clock();
 	for (int i = 0; i < num_of_thrd; i++) {
 
 		if (i == 0) {
@@ -65,12 +66,29 @@ int main() {
 		}
 
 	}
+	clock_t thrd_end_time = clock();
 
+	//Соединяем потоки
 	for (int i = 0; i < num_of_thrd; ++i) {
 		threads.at(i).join();
 	}
 
+	//Выводим результат и время выполнения параллельной функции
+	cout << "Thread sum = " << result << " \n";
+	double thrd_time_sec = (double)(thrd_end_time - thrd_start_time) / CLOCKS_PER_SEC;
+	cout << "Thread time = " << thrd_time_sec << " \n";
+
+	//Обнуляем результат и считаем стандартной функцией 
+	result = 0;
+	clock_t std_start_time = clock();
+	sum(result, arr, 0, arr_size);
+	clock_t std_end_time = clock();
+
+	//Выводим результат и время выполнения стандартной функции
+	cout << "std sum = " << result << " \n";
+	double std_time_sec = (double)(std_end_time - std_start_time) / CLOCKS_PER_SEC;
+	cout << "Std time = " << std_time_sec << " \n";
+
 	delete[] arr;
-	cout << result;
 
 }
